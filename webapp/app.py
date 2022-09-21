9ebcfb6c14d10b2cfd7ffabdacac99e470718873
emknowledge <aleydar1@gmx.de>
2022-09-21 12:17:23 +0200

    add edit and delete a post

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 def get_db_connection():
@@ -20,6 +20,8 @@ def get_post(post_id):
     return post
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
 
 @app.route('/')
 def index():
@@ -36,6 +38,57 @@ def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
 
+#view function that will render a template that displays a form you can fill in to create 
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
 
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+#View function for the post you edit is determined by the URL and Flask passes the ID number to the edit() function via the idForm you can fill in.
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+#view function will receive the ID of the post to be deleted from the URL
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
+    
 if __name__ =='__main__':
     app.run(port=1337 ,debug=True)
